cfb478c0adb449393ea88f3a63cf35c16cb705a7
NaveenDvlpr <115621342+NaveenDvlpr@users.noreply.github.com>
2023-04-26 18:52:38 -0700

    added power functionality

--- calculator.js
+++ calculator.js
@@ -9,3 +9,15 @@ console.log("subtraction: ",a-b);
 console.log("multiplication: ",a*b);
 
 console.log("division: ",a/b);
+
+const power = (a,b) => {
+  let ans = 1;
+  while(b){
+    if(b&1) ans*=a;
+    a*=a;
+    b>>=1;
+  }
+  return ans;
+}
+
+console.log("power of: ", power(a,b));
