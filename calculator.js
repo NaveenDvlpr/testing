9246221ae9eaffbf4e9c6f6d2a1aa548c9b72332
NaveenDvlpr <115621342+NaveenDvlpr@users.noreply.github.com>
2023-04-26 18:40:21 -0700

    added functionalities

--- calculator.js
+++ calculator.js
@@ -1,1 +1,11 @@
 console.log('Calculator App');
+
+let a = 3, b = 4;
+
+console.log("addition: ",a+b);
+
+console.log("subtraction: ",a-b);
+
+console.log("multiplication: ",a*b);
+
+console.log("division: ",a/b);
